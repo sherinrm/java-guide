6ff02c501c1315ae1116ef2d0ccb4655e75ec067
sherinrm <sherinrm@gmail.com>
2019-11-10 20:29:28 +0530

    Add datatypes.java

--- src/JavaBasics/DataTypes.java
+++ src/JavaBasics/DataTypes.java
@@ -1,9 +1,43 @@
 package JavaBasics;
 
 public class DataTypes {
-
+// java basics tutorial
+	/* this is the java basics
+	series covering most of the funamental conepts of java*/
+// main method is the starting or execution point of the program.
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+        //1. int
+		int i =10;
+		i = 20;
+		int j = 30;
+		int k =40;
+		int l = -1;
+		int w =0 ;
+
+		//2. Double
+		double d= 12.33;
+		double d1 =11.22;
+		double d2 =100;
+		//integer can be saved in double but double cannot be saved in integer
+
+		//3. character - sinlge digit value and should be written in single quotes
+		char c ='A';
+		char c1 ='a';
+		char c2 = '1';
+		// char c4 ='AA' is not allowed because character should be single digit value
+
+		// 4. boolean
+		boolean b1 = true;
+		boolean b2 = false;
+
+		// 5. String is class , not a data type but can be used as a
+
+
+
+
+
+		System.out.println("hello java world");
 
 	}
 
